df3dca3bf4ac3eb3234e0492ba6deac6f8bc8969
Annika Jusino <annika.jusino@gmail.com>
2017-01-31 17:55:38 -0500

    Update test.cpp
    More complex tests

--- test.cpp
+++ test.cpp
@@ -1,6 +1,21 @@
 #include "printf.hpp"
 
 int main(){
+	char buffer[100];
+	buffer[0] = 'a';
+//	buffer[1] = 's';
+//	buffer[2] = 'd';
+//	buffer[3] = 'f';
+//	buffer[4] = 'g';
+//	buffer[6] = '\0';
+//	buffer[5] = 'o';
+
 	int check = printf("test\n");
 	printf("check = %d\n", check);
+	printf("check = %x\n", 9223372036854775807);
+	printf("check = %d\n", 9223372036854775807);
+	printf("check = %f %f %f\n", 1.04, 12341.92093457, 0.000006);
+	snprintf(buffer, 100, "The half of %d is %d %s", 60, 60/2, "This is a practice sentence");
+
+	printf("%s\n", buffer);
 }
