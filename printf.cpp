f0e26d9b526333ecdcba1bb09c8678d3819a265a
Annika Jusino <annika.jusino@gmail.com>
2017-01-31 17:55:13 -0500

    Update printf.cpp
    Fixed "new", fixed rounding error in dtostr, added %s (doesn't end in % though), fixed 64-bit issue

--- printf.cpp
+++ printf.cpp
@@ -1,10 +1,10 @@
 #include "printf.hpp"
 
 //converts a decimal number into a string representation of its hexadecimal value
-char* hexconv(int64_t in){
+char* hexconv(long long in){
 	char converted[17]; //holds intial conversion of number from dec to hex
-	int64_t temp;		
-	int leadzero = 0; //used to track the number of leading zeroes on the converted string
+	long long temp;		
+	long long leadzero = 0; //used to track the number of leading zeroes on the converted string
 	
 	//converts the decimal value into a string representation of the hex value
 	for(int i = 0; i < 16; i++){
@@ -19,7 +19,7 @@ char* hexconv(int64_t in){
 	while (converted[leadzero] == '0')
 		leadzero++;
 
-	char *conv_short = new char[17 - leadzero]; //holds the converted string minus leading zeroes
+	char conv_short[17 - leadzero]; //holds the converted string minus leading zeroes
 	
 	//removes leading zeroes, placing final representation into cov_short
 	for(int i = 0; i < 17 - leadzero -1; i++){
@@ -39,8 +39,8 @@ int cwrite(const char* wstr) //writes string of variable size
 	char cur = wstr[0];
 
 	while(cur != '\0'){
-		size++;
 		cur = wstr[size];
+		size++;
 	}
 
 	return write(1, wstr, size);
@@ -73,12 +73,12 @@ int scwrite(const char* wstr, char* dest, int index, int max){
 
 //converts integer into string
 
-const char* itostr(int tostr)
+const char* itostr(long long tostr)
 {
-	int whole;
-	int digit;
-	char* wstr = new char[100]();			//stands for "whole" string
-	char* mystr = new char[100]();
+	long long whole;
+	long long digit;
+	char wstr[100];			//stands for "whole" string
+	char mystr[100];
 	int index = 0;
 	int j = 0;
 
@@ -102,25 +102,34 @@ const char* itostr(int tostr)
 		mystr[j] = wstr[index - j - 1];
 		j++;
 	}
-	
+
+	mystr[j] = '\0';
+
+
 	return mystr;
 }
 
 //converts doubles to strings--same as itostr except for commented sections
 
-const char* dtostr(double tostr)	//Needs formatting/precision (default 6) and roundingw
+const char* dtostr(double tostr)//, int b4, int after)	//Needs formatting/precision (default 6) and roundingw
 {
 	int whole;
 	int digit;
 	double zeroplus = 0;			//keeps track of the "whole number" part of the number--subtracted later to obtain the decimal part only
 	double decimal;
-	char* wstr = new char[100]();
-	char* mystr = new char[100]();
+	char wstr[100];
+	char mystr[100];
 	int index = 0;
 	int i = 0;						//used for counting up the power of 10
 	int j = 0;
 	int prec = 6;
 	int power;						//also used for counting up the power of 10
+/*
+	if(after != -1)
+	{
+		prec = after;
+	}
+*/
 
 	whole = tostr / 1;				//integer divide for the sake of whole #'s
 	
@@ -165,25 +174,31 @@ const char* dtostr(double tostr)	//Needs formatting/precision (default 6) and ro
 	while(i < prec)		//has a rounding error somewhere, works opposite to the idea earlier; multiplies by 10 instead
 	{
 		decimal = decimal * 10.0;
-		digit = (int)decimal % 10;
-		mystr[index] = (char) (digit+48);
-		index++;
+		mystr[index] = (char) (decimal+48);				//.whatever * 10 = w.hatever, take w + 48 (corresponding char)
+		decimal = decimal - int(mystr[index] - 48);		//take w-48 to get original integer, subtract w to get .hatever
+		index++;										//scoot forward and continue
 		i++;
 
 	}
+
+	mystr[index] = '\0';
 	return mystr;
 }
 
 
 int printf(const char *fmt, ...){
 	va_list args;				//args list
-	int tempi;					//integer buffer for args
+	long long tempi;					//integer buffer for args
 	double tempf;				//float/double buffer for args
 	int size = 0;				
 	char cur = fmt[0];			//current character
-	char *buf = new char[1]();	//for writing letter by letter
+	char buf[1];	//for writing letter by letter
 	const char* converted;		//buffer for numbers converted to strings
 	int count = 0;				//tracks number of characters written to output for returning from printf
+	char *pstring;
+
+//	int b4 = 0;
+//	int after = 0;
 
 	va_start(args, fmt);		//init list to arg named "fmt"
 	
@@ -200,31 +215,52 @@ int printf(const char *fmt, ...){
 			i++;			
 
 									//pull next arg of that data type, convert, and print
+			/*						
+			if(fmt[i] != ('d' || 'f' || 'x'))
+			{
+				while(fmt[i] != '.')
+				{
+					b4 = b4 * 10;
+					b4 = b4 + (char)(fmt[i] + 48);
+					i++;
+				}
+
+				i++;
+
+				while(fmt[i] != 'f')
+				{
+					after = after * 10;
+					after = (char)(fmt[i] + 48);
+					i++
+				}
+
+			}
+			*/
 			
 			if(fmt[i] == 'd')
 			{
 //				cwrite(" integer ");
-				tempi = va_arg(args, int);
+				tempi = va_arg(args, long long);
 				converted = itostr(tempi);
 				count += cwrite(converted);
 			}else if(fmt[i] == 'x')
 			{
 //				write(1,"hex",5);
-				converted = hexconv(va_arg(args, int));
+				converted = hexconv(va_arg(args, long long));
 				count += cwrite(converted);
 			}else if(fmt[i] == 'f')
-				
+					
 			//may need additional i++'s to get all of the formatting flags in
 			//may need encasing "while != any of these...(d, x, f, s, etc)" to collect flags before %f
-			
-			
-			
-			
 			{
 //				cwrite("float");
 				tempf = va_arg(args, double);
 				converted = dtostr(tempf);
 				count += cwrite(converted);
+			}else if(fmt[i] == 's')
+			{
+				pstring = va_arg(args, char*);
+				cwrite(pstring);
 			}
 		}
 		else						//if not % sign, write as usual
@@ -242,13 +278,14 @@ int printf(const char *fmt, ...){
 
 int snprintf(char *dest, size_t size, const char *fmt, ...){
 	va_list args;				//args list
-	int tempi;					//integer buffer for args
+	long long tempi;					//integer buffer for args
 	double tempf;				//float/double buffer for args
-	int asize = size-1;			//number of non-termination characters that can be written
+	long long asize = size-1;			//number of non-termination characters that can be written
 	const char* converted;		//buffer for numbers converted to strings
-	int count = 0;				//tracks number of characters written to dest
+	long long count = 0;				//tracks number of characters written to dest
 	va_start(args, fmt);		//init list to arg named "fmt"
-	int i = 0;					//iteration variable
+	long long i = 0;					//iteration variable
+	char *pstring;
 
 	while(fmt[i] != '\0' && count < asize)
 	{
@@ -259,12 +296,12 @@ int snprintf(char *dest, size_t size, const char *fmt, ...){
 			//pull next arg of that data type, convert, and print
 			if(fmt[i] == 'd')
 			{
-				tempi = va_arg(args, int);
+				tempi = va_arg(args, long long);
 				converted = itostr(tempi);
 				count += scwrite(converted, dest, count, asize );
 			}else if(fmt[i] == 'x')
 			{
-				converted = hexconv(va_arg(args, int));
+				converted = hexconv(va_arg(args, long long));
 				count += scwrite(converted, dest, count, asize);
 			}else if(fmt[i] == 'f')
 			//may need additional i++'s to get all of the formatting flags in
@@ -273,6 +310,10 @@ int snprintf(char *dest, size_t size, const char *fmt, ...){
 				tempf = va_arg(args, double);
 				converted = dtostr(tempf);
 				count += scwrite(converted, dest, count, asize);
+			}else if(fmt[i] == 's')
+			{
+				pstring = va_arg(args, char*);
+				count += scwrite(pstring, dest, count, asize);
 			}
 		}
 		else						//if not % sign, write as usual
@@ -287,4 +328,3 @@ int snprintf(char *dest, size_t size, const char *fmt, ...){
 
 	return count;
 }
-
